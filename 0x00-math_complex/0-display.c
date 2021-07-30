548a31eef798952e5c0c5a710dc3d5b94c8961a6
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-30 18:31:49 -0500

    file display modified

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -12,7 +12,7 @@ void display_complex_number(complex c)
 	else if (c.re == 0)
 	printf("%.0fi\n", c.im);
 	else if (c.re == 0 && c.im == 0)
-	printf("nill\n");
+	printf("(null)\n");
 	else if (c.im < 0)
 	printf("%.0f - %.0fi\n", c.re, c.im * -1);
 	else
