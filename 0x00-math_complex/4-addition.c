d251cce458cc238480af01136b1f5bd09e6a9892
IsabelaGomez101 <3379@holbertonschool.com>
2021-07-30 22:57:07 -0500

    add file addition

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <math.h>
+
+/**
+ * addition - function that performs the addition operation to complex numbers.
+ * @c1: complex number 1.
+ * @c2: complex number 2.
+ * @c3: pointer complex number.
+ * Return: void.
+ */
+
+void addition(complex c1, complex c2, complex *c3)
+{
+complex c4;
+
+c4.re = (c1.re) + (c2.re);
+c4.im = (c1.im) + (c2.im);
+
+*c3 = c4;
+}
